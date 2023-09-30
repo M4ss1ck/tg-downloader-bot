941f28b67dd5acf148b7a27b9de3925fcd93283d
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-29 21:49:18 -0400

    add /downloads command

--- src/middleware/commands.ts
+++ src/middleware/commands.ts
@@ -3,14 +3,14 @@ import { prisma } from "../db/prisma.js";
 import { logger } from "../logger/index.js";
 import { csvExport } from "../utils/csvExport.js";
 import { unlink } from "fs/promises";
+import { downloader } from "../queues/download.js";
 
 export const commands = new Composer()
 
-commands.command('get', async ctx => {
-    if (ctx.chat.type === 'private') {
-        const username = ctx.message.text.replace(/^\/get(@\w+)?/, '').trim()
-
-    }
+commands.command(['dls', 'downloads'], async ctx => {
+    const counts = await downloader.getJobCounts('wait', 'completed', 'failed');
+    // Returns an object like this { wait: number, completed: number, failed: number }
+    ctx.sendMessage(`<pre>${JSON.stringify(counts, null, 2)}</pre>`, { parse_mode: "HTML" })
 })
 
 commands.command('myid', async ctx => {
