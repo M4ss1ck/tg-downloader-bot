69144dfaa3d3f632cd8f41e2cbc2e1afb77be776
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-30 09:41:09 -0400

    remove conflicting code

--- src/middleware/actions.ts
+++ src/middleware/actions.ts
@@ -13,42 +13,5 @@ actions.action(/^exportAll_\d+/i, async ctx => {
             await ctx.answerCbQuery('this is not your data')
         }
         await ctx.answerCbQuery().catch(logger.error)
-
-        const result = await prisma.user.findUnique({
-            where: {
-                tgId: userId
-            },
-            select: {
-                twitterCircle: true
-            }
-        })
-
-        if (result && result.twitterCircle.length > 0) {
-            const path = `Followers_${userId}.csv`
-            const data = result.twitterCircle.map(user => ({
-                twId: user.twId,
-                name: user.name,
-                username: `@${user.username}`,
-                follower: user.follower ? "Yes" : "No",
-                following: user.following ? "Yes" : "No",
-            }))
-            const headers = [
-                { id: 'twId', title: 'Twitter ID' },
-                { id: 'name', title: 'Name' },
-                { id: 'username', title: 'Twitter handle' },
-                { id: 'follower', title: 'Follower' },
-                { id: 'following', title: 'Following' },
-            ]
-            const success = await csvExport(path, data, headers)
-            if (success) {
-                await ctx.replyWithDocument((Input.fromLocalFile(path)), {
-                    caption: 'List of accounts related to you'
-                }).catch(logger.error)
-                await unlink(path)
-            }
-        }
-        else {
-            ctx.reply('No info in database!')
-        }
     }
 })
