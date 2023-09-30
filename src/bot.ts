26853bebd24fa458a9a238c4a58bc308abaadd0f
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-30 09:42:41 -0400

    use the new middleware and set apiRoot

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,17 @@
 import { Telegraf } from "telegraf";
-import { app } from "./server/app.js";
-import { logger } from "./logger/index.js";
-import { TOKEN, PORT } from "./config/index.js";
+import { TOKEN, LOCAL_API } from "./config/index.js";
 import { validator } from "./middleware/validator.js";
 import { start } from "./middleware/start.js";
 import { commands } from "./middleware/commands.js";
 import { actions } from "./middleware/actions.js";
 import { admin } from "./middleware/admin.js";
-import { worker } from "./queues/download.js";
+import { files } from "./middleware/files.js";
 
 export const bot = new Telegraf(TOKEN, {
-    handlerTimeout: 9000000
+    handlerTimeout: 9000000,
+    telegram: {
+        apiRoot: LOCAL_API
+    }
 });
 
 bot
@@ -19,4 +20,5 @@ bot
     .use(start)
     .use(commands)
     .use(actions)
+    .use(files)
 
