65487c224d541ed4c851abfc8259b4af51101743
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-30 19:27:01 -0400

    fix link in response message

--- src/utils/downloader.ts
+++ src/utils/downloader.ts
@@ -5,6 +5,7 @@ import { prisma } from "../db/prisma.js"
 import { cp, unlink } from "fs/promises"
 import { __dirname } from "./getSize.js"
 import { getConfig } from "./config.js"
+import { URL_PREFIX } from "../config/index.js"
 
 export const downloadFile = async (job: Job) => {
     logger.info('Processing job ' + job.id)
@@ -18,14 +19,14 @@ export const downloadFile = async (job: Job) => {
             const index = String(url).lastIndexOf('/')
             filename = String(url).substring(index + 1)
         }
-        const link = __dirname + "/Downloads/" + filename
-        await cp(url, link)
-        await unlink(url)
+        await cp(url, __dirname + "/Downloads/" + filename)
+        const link = URL_PREFIX + "/dl/" + filename
         logger.success('Link: ' + link)
         await bot.telegram.sendMessage(user, `File uploaded, you can find it <a href="${link}">here</a>`, {
             parse_mode: "HTML",
             disable_web_page_preview: true,
         })
+        await unlink(url)
     } catch (error) {
         logger.error(error)
         throw new Error('Error in downloadFile()')
