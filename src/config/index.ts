7596b94b6892e5abc27fbf28e35dd53ad3f8cea4
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-30 16:00:27 -0400

    add URL_PREFIX variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,4 @@ export const ADMIN_ID = process.env.ADMIN_ID ?? ""
 export const TOKEN = process.env.BOT_TOKEN ?? ""
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000
 export const LOCAL_API = process.env.LOCAL_API ?? "http://localhost:8081"
+export const URL_PREFIX = process.env.URL_PREFIX?.replace(/\/$/g, '') ?? "http://localhost:3000"
