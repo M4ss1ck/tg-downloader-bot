e0a35b450833621ace37a2cc112ac670f71d1909
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-10-01 12:18:38 -0400

    add updateCurrentSize() function

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,3 +7,26 @@ export const getConfig = async () => {
         }
     })
 }
+
+/**
+ * Update the value of currentSize (GB) in the database
+ * @param value size in bytes
+ * @param negative this is telling us if we need to increment or decrement. True for decrements, false by default.
+ * @returns 
+ */
+export const updateCurrentSize = async (value: number, negative = false) => {
+    let increment = Number(value / Math.pow(1024, 3))
+    if (negative) {
+        increment *= -1
+    }
+    return prisma.config.update({
+        where: {
+            id: 'global'
+        },
+        data: {
+            currentSize: {
+                increment: value,
+            }
+        }
+    })
+}
