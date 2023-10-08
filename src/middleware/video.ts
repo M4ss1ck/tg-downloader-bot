9903aea7b0eb6bd18b5ec42250e9e91dc1fd1d74
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-10-08 12:18:45 -0400

    add url listener

--- src/middleware/video.ts
+++ src/middleware/video.ts
@@ -7,48 +7,18 @@ import { logger } from "../logger/index.js"
 
 export const video = new Composer()
 
-video.command(['yt', 'video'], async (ctx) => {
-    if (ctx.chat.type === 'private') {
-        try {
-            const user = ctx.message.from
-            const [url, name] = ctx.message.text.replace(/^\/(dl|yt|video)\s+/, '').split(' ')
-            // TODO: validate url
-            const downloadObject = {
-                name: name ?? `${user.id}.mp4`,
-                user: user.id,
-                url,
-                type: 'video'
-            }
-            const job = await downloader.add(`${user.id}-${name ?? Date.now()}`, { data: downloadObject })
-            console.log(job.data)
-            return ctx.replyWithHTML('Link was added to queue')
-        } catch (error) {
-            console.error(error)
-            return ctx.replyWithHTML('I forgot my cat in the fridge and couldn\'t add the file to queue')
-        }
-    }
-})
-
 video.command(['dl', 'url'], async ctx => {
     if (ctx.chat.type === 'private') {
         try {
             const user = ctx.message.from
             const [url, name] = ctx.message.text.replace(/^\/(dl|url)\s+/, '').split(' ')
             // TODO: validate url
-            let size = 0
-            // const downloadObject = {
-            //     name: name ?? `${user.id}.mp4`,
-            //     size,
-            //     user: user.id,
-            //     url,
-            //     type: 'video'
-            // }
             const info = await ytdl.getInfo(url)
             const btnLabels = info.formats.map(f => `[${f.container}]${f.hasVideo ? `[${f.qualityLabel}] ${f.height ?? 'n/a'}x${f.width ?? 'n/a'} ` : ''} Video: ${f.hasVideo ? '✅' : '❌'} Audio: ${f.hasAudio ? '✅' : '❌'} (${convertBytes(Number(f.contentLength ?? '0'))})`)
             const iTags = info.formats.map(f => `${f.itag}_${f.container}`)
             console.log(info)
             // add to database to fetch a given format later
-            const { id } = await addTempLink(url, String(user.id))
+            const { id } = await addTempLink(url, String(user.id), info.videoDetails.title)
 
             const buttons = btnLabels.map((label, i) => [Markup.button.callback(label, `dl_${id}_${iTags[i]}`)])
             const keyboard = Markup.inlineKeyboard(buttons)
@@ -83,5 +53,32 @@ video.action(/dl_(\d+)_(\d+)/, async ctx => {
         const job = await downloader.add(`${ctx.callbackQuery.data}`, { data: downloadObject })
         console.log(job.data)
         return ctx.answerCbQuery('Download was added to queue.', { show_alert: true }).catch(logger.error)
+    }
+})
+
+const urls = /^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?$/
+
+video.hears(urls, async ctx => {
+    if (ctx.chat.type === 'private') {
+        try {
+            const user = ctx.message.from
+            const url = ctx.message.text
+            const info = await ytdl.getInfo(url)
+            const btnLabels = info.formats.map(f => `[${f.container}]${f.hasVideo ? `[${f.qualityLabel}] ${f.height ?? 'n/a'}x${f.width ?? 'n/a'} ` : ''} Video: ${f.hasVideo ? '✅' : '❌'} Audio: ${f.hasAudio ? '✅' : '❌'} (${convertBytes(Number(f.contentLength ?? '0'))})`)
+            const iTags = info.formats.map(f => `${f.itag}_${f.container}`)
+            console.log(info)
+            // add to database to fetch a given format later
+            const { id } = await addTempLink(url, String(user.id), info.videoDetails.title)
+
+            const buttons = btnLabels.map((label, i) => [Markup.button.callback(label, `dl_${id}_${iTags[i]}`)])
+            const keyboard = Markup.inlineKeyboard(buttons)
+            // const job = await downloader.add(`${user.id}-${name ?? Date.now()}`, { data: downloadObject })
+            // console.log(job.data)
+            const text = `${info.videoDetails.title ?? 'Title n/a'}\n\nList of available formats:\n${btnLabels.join('\n')}`
+            return ctx.replyWithHTML(text, keyboard)
+        } catch (error) {
+            console.error(error)
+            return ctx.replyWithHTML('I forgot my cat in the fridge and couldn\'t add the file to queue')
+        }
     }
 })
